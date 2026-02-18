7b25b0254b5dcd9c0b759d843e6a820ccd97e41f
yasmina <yasminwoodlock@gmail.com>
2026-02-18 16:49:08 +0000

    shows list of what's in database

--- app/src/main/java/com/example/nugget/data/db.kt
+++ app/src/main/java/com/example/nugget/data/db.kt
@@ -5,6 +5,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
+import kotlinx.coroutines.tasks.await
 
 // Define your data class for a food item
 data class FoodItem(
@@ -12,23 +13,11 @@ data class FoodItem(
     val dateBought: String? = null,
     val expiryDate: String? = null
 )
-fun getFoodItems() {
+suspend fun getFoodItems(): List<FoodItem> {
     val database = Firebase.database("https://mad-food-storage-default-rtdb.europe-west1.firebasedatabase.app/")
-    val myRef = database.getReference("foodItems")
+    val snapshot = database.getReference("foodItems").get().await()
 
-    myRef.addValueEventListener(object : ValueEventListener {
-        override fun onDataChange(snapshot: DataSnapshot) {
-            val foodItemsList = mutableListOf<FoodItem>()
-            for (foodSnapshot in snapshot.children) {
-                val foodItem = foodSnapshot.getValue(FoodItem::class.java)
-                foodItem?.let { foodItemsList.add(it) }
-            }
-            println("Food Items: $foodItemsList")
-        }
-
-        override fun onCancelled(error: DatabaseError) {
-            // Handle error
-            println("Failed to read value: ${error.toException()}")
-        }
-    })
+    return snapshot.children.mapNotNull {
+        it.getValue(FoodItem::class.java)
+    }
 }
